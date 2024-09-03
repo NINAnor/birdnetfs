92bcb76f343c54abc4661ff439e1c1939cc286e5
Benjamin Cretois <benjamin.cretois@nina.no>
2024-09-03 12:27:25 +0200

    [ADD] remove_extensions

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ import shutil
 import fsspec
 import librosa
 import audioread
+import glob
 
 def read_file(filepath, sr):
     with fsspec.open(filepath) as f:
@@ -28,3 +29,69 @@ def clean_tmp(directory="/tmp"):
                     print(f"Deleted folder: {folder_path}")
             except Exception as e:
                 print(f'Failed to delete {folder_path}. Reason: {e}')
+
+def openCachedFile(filesystem, path, sample_rate=48000, offset=0.0, duration=None):
+    
+    import tempfile
+    import shutil
+
+    bin = filesystem.openbin(path)
+
+    with tempfile.NamedTemporaryFile() as temp:
+        shutil.copyfileobj(bin, temp)
+        sig, rate = openAudioFile(temp.name, sample_rate, offset, duration)
+
+    return sig, rate
+
+def openAudioFile(path, sample_rate=44100, offset=0.0, duration=None):    
+
+    try:
+        sig, rate = librosa.load(path, sr=sample_rate, offset=offset, duration=duration, mono=True, res_type='kaiser_fast')
+    except:
+        sig, rate = [], sample_rate
+
+    return sig, rate
+
+def saveSignal(sig, fname):
+
+    import soundfile as sf
+    sf.write(fname, sig, 48000, 'PCM_16')
+
+#####################################################################
+######################### PARSING UTILS #############################
+#####################################################################
+
+def remove_extension(input):
+    filename = input.split("/")[-1].split(".")[0]
+    if len(filename) > 2:
+        filename = ".".join(filename[0:-1])
+    else:
+        filename = input.split("/")[-1].split(".")[0]
+    return filename
+
+def parseFolders(apath, rpath):
+
+    audio_files = [f for f in glob.glob(apath + "/**/*", recursive = True) if os.path.isfile(f)]
+    audio_no_extension = []
+    for audio_file in audio_files:
+        audio_file_no_extension = remove_extension(audio_file)
+        audio_no_extension.append(audio_file_no_extension)
+
+
+    result_files = [f for f in glob.glob(rpath + "/**/*", recursive = True) if os.path.isfile(f)]
+
+    flist = []
+    for result in result_files:
+        result_no_extension = remove_extension(result)
+        is_in = result_no_extension in audio_no_extension
+
+        if is_in:
+            audio_idx = audio_no_extension.index(result_no_extension)
+            pair = {'audio': audio_files[audio_idx], 'result': result}
+            flist.append(pair)
+        else:
+            continue
+
+    print('Found {} audio files with valid result file.'.format(len(flist)))
+
+    return flist

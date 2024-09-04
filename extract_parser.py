13c2bd72594fa88578efca6c73aa598a453d2aff
Benjamin Cretois <benjamin.cretois@nina.no>
2024-09-04 11:00:24 +0200

    [ADD] parralelisation to extract

--- extract_parser.py
+++ extract_parser.py
@@ -7,11 +7,12 @@ import yaml
 import fs
 import sys
 import logging
+import pyarrow as pa
+import pyarrow.parquet as pq
 import json
 from tenacity import wait_exponential, retry
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from yaml import FullLoader
-import pyarrow.parquet as pq
 
 import config as cfg
 from utils import remove_extension, openCachedFile, openAudioFile, saveSignal
@@ -121,68 +122,33 @@ def find_segments(audio_file, result_file, confidence_threshold):
 
     return segments
 
-#@retry(wait=wait_exponential(multiplier=5, min=60, max=600))
-def extract_segments(item, sample_rate, out_path, filesystem, seg_length=3):
-    """Extract segments from the audio file and save them with retries."""
-    segments = item
-    audio_file = item["audio"]
 
-    try:
-        signal, rate = (openAudioFile(audio_file, sample_rate) if not filesystem else openCachedFile(filesystem, audio_file, sample_rate))
-        save_extracted_segments(signal, rate, segments, out_path, seg_length)
-        logging.info(f'Segments extracted from {audio_file}')
-    except Exception as e:
-        logging.error(f"Failed to extract segments from {audio_file}: {e}")
-        logging.error(traceback.format_exc())
-        raise
 
-def save_extracted_segments(signal, rate, segment, out_path, seg_length):
-    """Save the extracted segments to the output path."""
-    #for segment in segments:
-    try:
-        start = int(segment['start'] * rate)
-        end = int(segment['end'] * rate)
-        offset = ((seg_length * rate) - (end - start)) // 2
-        start, end = max(0, start - offset), min(len(signal), end + offset)
-
-        if end > start:
-            segment_signal = signal[start:end]
-            save_segment(segment_signal, segment, out_path)
-
-    except Exception as e:
-        logging.error(f"Error saving segment {segment}: {e}")
-        logging.error(traceback.format_exc())
-
-def save_segment(segment_signal, segment, out_path):
-    """Save an individual segment."""
-    species_path = os.path.join(out_path, segment['species'])
-    os.makedirs(species_path, exist_ok=True)
-
-    segment_name = f"start={segment['start']}_end={segment['end']}_conf={segment['confidence']:.3f}_file={os.path.basename(segment['audio']).rsplit('.', 1)[0]}.wav"
-    segment_path = os.path.join(species_path, segment_name)
-    saveSignal(segment_signal, segment_path)
-
-if __name__ == '__main__':
-
+if __name__ == "__main__":
 
     setup_logging()
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--config', default='config_connection.yaml', help='Path to the configuration file.')
-    parser.add_argument('--parquet_file', default='sample.parquet')
-    parser.add_argument('audio_file')
     args = parser.parse_args()
 
     with open(args.config, 'r') as config_file:
         config = yaml.load(config_file, Loader=yaml.FullLoader)
 
     myfs = do_connection(config['CONNECTION_STRING'])
- 
-    items = pq.read_table(args.parquet_file, filters=[["audio", "=", args.audio_file]])
+    parsed_folders = parse_folders(myfs, config['INPUT_PATH'], config['OUTPUT_PATH'])
+    parsed_files = parse_files(parsed_folders, config['NUM_SEGMENTS'], config['THRESHOLD'])
 
-    for item in items.to_pylist():
-        extract_segments(item, 
-                         config["SAMPLE_RATE"], 
-                         config["OUT_PATH_SEGMENTS"], 
-                         myfs, 
-                         seg_length=3)
+    entries = [item[1][0] for item in parsed_files]
+    entries.sort(key=lambda e: e["audio"])
+
+
+    table = pa.table({
+        "audio": (entry["audio"] for entry in entries),
+        "start": (entry["start"] for entry in entries),
+        "end": (entry["end"] for entry in entries),
+        "species": (entry["species"] for entry in entries),
+        "confidence": (entry["confidence"] for entry in entries),
+    })
+
+    pq.write_table(table, "sample.parquet")
